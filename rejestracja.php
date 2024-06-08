683927cbde560e28c3187dadc1103bf6cf6e1a75
Kiersna <kiersnowskid@gmail.com>
2024-06-08 11:10:46 +0200

    frontend rejestacji

--- rejestracja.php
+++ rejestracja.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="logowanie-rejestracja.css">
+    <link rel="stylesheet" href="logowanie_rejestracja.css">
     <script src="https://www.google.com/recaptcha/enterprise.js?render=6LcgRPMpAAAAABVYEld96OO9m0KiEbfm9TgLFumD"></script>
 </head>
 <body>
+    <h1>Rejestracja</h1>
+    <div class="formularz">
     <form action="" id='rejestracja_formularz'>
         <input type="text" placeholder="Imie" id="imie">
         <input type="text" placeholder="Nazwisko" id="nazwisko">
@@ -21,9 +23,13 @@
             data-sitekey="6LcgRPMpAAAAABVYEld96OO9m0KiEbfm9TgLFumD"
             data-callback='onSubmit'
             data-action='submit'>
-            Submit
+            Zaloguj się
         </button>
     </form>
+    </div>
+    <footer>
+        <p>Ogloszenia24</p>
+    </footer>
 </body>
 <!-- Replace the variables below. -->
 <script>
